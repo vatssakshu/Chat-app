6095ca3e962c6163ec9b50c6bb1450f0cce88b85
Saksham <vatssakshu@gmail.com>
2024-04-11 23:51:20 +0530

    created message endpoints

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const sendMessage = async (req, res, next) => {
   try {
@@ -30,6 +31,23 @@ export const sendMessage = async (req, res, next) => {
 
     await Promise.all([conversation.save(), newMessage.save()]);
     res.status(201).json(newMessage);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); //Not the Reference but the actual message
+
+    if (!conversation) return next(errorHandler(200, ""));
+
+    const messages = conversation.messages;
+    res.status(200).json(messages);
   } catch (error) {
     next(error);
   }
